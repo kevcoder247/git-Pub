db31e98e62e077cee1154248bf8e1b63e4745e69
kevin ventura <kevcoder@kevins-mbp.myfiosgateway.com>
2023-05-31 17:37:15 -0400

    wip:food data added, and linked to the index page. we can also view individual dishes when we click the link

--- server.js
+++ server.js
@@ -2,13 +2,16 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 const drinks = require('./models/drinks');
+const food = require('./models/food')
 
 
 
 
 //Index Route
 app.get('/drinks/', (req, res) => {
-  res.render('drinks_index.ejs', {drinks});
+  res.render('drinks_index.ejs', {drinks, food});
+
+  console.log(food[0].name)
 });
 
 
@@ -17,7 +20,14 @@ app.get('/drinks/:id', (req, res) => {
   res.render('drinks_show.ejs', {
     drinks: drinks[req.params.id]
   })
+});
+
+app.get('/food/:id', (req, res) => {
+  res.render('food_show.ejs',{
+    food: food[req.params.id]
+  } )
 })
+
 
 app.listen(PORT, () => {
   console.log(`App is listening on port ${PORT}`)
